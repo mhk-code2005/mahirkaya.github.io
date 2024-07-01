104054d6c15d0459622d6d2092efd917619663ce
mhk-code2005 <76763225+mhk-code2005@users.noreply.github.com>
2024-07-01 12:59:38 -0500

    Initial commit

---
+++ website/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.home, name = "home"),
+    path('research/', views.research, name='research'),
+    path('resume/', views.resume, name='resume'),
+    path('projects/', views.projects, name='projects'),
+    path('bookrec/', views.bookrec, name='bookrec'),
+
+]
---
+++ website/views.py
@@ -0,0 +1,75 @@
+from django.shortcuts import render, HttpResponse
+import requests
+
+# Create your views here.
+def home(request):
+    return render(request, "home.html")
+
+def research(request):
+    return render(request, 'research.html')
+
+def resume(request):
+    return render(request, 'resume.html')
+
+def projects(request):
+    return render(request, 'projects.html')
+
+def bookrec(request):
+    query = request.GET.get('query')
+    context = {'query': query}
+
+    if query:
+        response = requests.get(f"http://172.17.0.2:6000/search?query={query}")
+        data = response.json()
+
+class Book:
+    def __init__(self, title, author, published_date, description, isbn, url):
+        self.title = title
+        self.author = author
+        self.published_date = published_date
+        self.description = description
+        self.isbn = isbn
+        self.url = url
+
+# views.py
+from django.shortcuts import render
+import requests
+
+class Book:
+    def __init__(self, title, rating, url, description, author, genres):
+        self.title = title
+        self.author = author
+        self.description = description[0:500] + "..."
+        self.rating = rating
+        self.url = url
+        self.genres = genres
+
+def bookrec(request):
+    query = request.GET.get('query')
+    count = request.GET.get('count', 10)  # Default to 10 if not specified
+    context = {'query': query, 'count': count}
+
+    if query:
+        url = 'http://127.0.0.1:6000/input'
+        data = {'text': query, 'count': count}
+        response = requests.post(url, json=data)
+
+        if response.status_code == 200:
+            result = response.json()['result']
+            if result[1] == 0:
+                suggestions = result[0]  # List of book title suggestions
+                context.update({
+                    'code': 0,
+                    'suggestions': suggestions
+                })
+            else:
+                books_data = result[0]
+                books = [Book(b[0], b[1], b[2], b[3], b[4], b[5].replace("[", "").replace("]", "")) for b in books_data]
+                context.update({
+                    'code': 1,
+                    'books': books
+                })
+        else:
+            context.update({'error': 'Error fetching data from server'})
+
+    return render(request, 'bookrec.html', context)
