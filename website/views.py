17984aabc5d9e31dd995b65d9280fa9d7caf0926
mhk-code2005 <76763225+mhk-code2005@users.noreply.github.com>
2024-07-01 20:45:45 -0500

    Fix STATIC_ROOT configuration for collectstatic

--- website/views.py
+++ website/views.py
@@ -31,10 +31,12 @@ def bookrec(request):
     context = {'query': query, 'count': count}
 
     if query:
-        url = 'http://0.0.0.0:6000/input'
-        data = {'text': query, 'count': count}
-        response = requests.post(url, json=data)
-
+        try:
+            url = 'http://0.0.0.0:6000/input'
+            data = {'text': query, 'count': count}
+            response = requests.post(url, json=data)
+        except:
+            context.update({'error': 'Error connecting to server'})
         if response.status_code == 200:
             result = response.json()['result']
             if result[1] == 0:
